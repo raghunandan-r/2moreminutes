2c1ba7c6921d78d2f9a56c226bee07675aa2d41d
Raghunandan <raghunandan092@gmail.com>
2024-08-20 21:56:28 -0400

    todo list interface updates

--- popup/popup.js
+++ popup/popup.js
@@ -1,45 +1,58 @@
-document.getElementById('addTodo').addEventListener('click', addTodo);
-
-function addTodo() {
-  const todoText = document.getElementById('newTodo').value;
-  if (todoText) {
-    chrome.storage.sync.get({todos: []}, function(data) {
-      const todos = data.todos;
-      todos.push({text: todoText, completed: false});
-      chrome.storage.sync.set({todos: todos}, function() {
-        renderTodos();
-      });
-    });
-  }
-}
-
-function renderTodos() {
-  chrome.storage.sync.get({todos: []}, function(data) {
+document.addEventListener('DOMContentLoaded', function() {
+    const addTodoButton = document.getElementById('addTodo');
+    const newTodoInput = document.getElementById('newTodo');
     const todoList = document.getElementById('todoList');
-    todoList.innerHTML = '';
-    data.todos.forEach((todo, index) => {
-      const li = document.createElement('li');
-      li.textContent = todo.text;
-      if (todo.completed) {
-        li.style.textDecoration = 'line-through';
-      }
-      li.addEventListener('click', () => toggleTodo(index));
-      todoList.appendChild(li);
-    });
-  });
-}
 
-function toggleTodo(index) {
-  chrome.storage.sync.get({todos: []}, function(data) {
-    const todos = data.todos;
-    todos[index].completed = !todos[index].completed;
-    chrome.storage.sync.set({todos: todos}, function() {
-      renderTodos();
-      if (todos[index].completed) {
-        chrome.runtime.sendMessage({action: 'taskCompleted'});
-      }
+    addTodoButton.addEventListener('click', addTodo);
+    newTodoInput.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter') {
+            addTodo();
+        }
     });
-  });
-}
 
-renderTodos();
+    function addTodo() {
+        const todoText = newTodoInput.value.trim();
+        if (todoText) {
+            chrome.storage.sync.get({todos: []}, function(data) {
+                const todos = data.todos;
+                todos.push({text: todoText, completed: false});
+                chrome.storage.sync.set({todos: todos}, function() {
+                    renderTodos();
+                    newTodoInput.value = '';
+                });
+            });
+        }
+    }
+
+    function renderTodos() {
+        chrome.storage.sync.get({todos: []}, function(data) {
+            todoList.innerHTML = '';
+            data.todos.forEach((todo, index) => {
+                const li = document.createElement('li');
+                li.className = 'todo-item' + (todo.completed ? ' completed' : '');
+                li.innerHTML = `
+                    <input type="checkbox" class="todo-checkbox" ${todo.completed ? 'checked' : ''}>
+                    <span class="todo-text">${todo.text}</span>
+                `;
+                const checkbox = li.querySelector('.todo-checkbox');
+                checkbox.addEventListener('change', () => toggleTodo(index));
+                todoList.appendChild(li);
+            });
+        });
+    }
+
+    function toggleTodo(index) {
+        chrome.storage.sync.get({todos: []}, function(data) {
+            const todos = data.todos;
+            todos[index].completed = !todos[index].completed;
+            chrome.storage.sync.set({todos: todos}, function() {
+                renderTodos();
+                if (todos[index].completed) {
+                    chrome.runtime.sendMessage({action: 'taskCompleted'});
+                }
+            });
+        });
+    }
+
+    renderTodos();
+});
